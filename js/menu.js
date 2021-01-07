88354c6b8f12d156613f50769cffcc6471b25c17
Milan <milanjulinac996@gmail.com>
2021-01-07 23:00:01 +0100

    modified some files

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,16 @@ const foodButton = document.querySelector(".food-btn");
 const drinkItems = document.querySelector(".coffe-drinks");
 const foodItems = document.querySelector(".food-items");
 
+const menuItems = document.querySelector(".menu-items");
+const itemsSection = document.querySelector(".items-section");
+
 foodButton.addEventListener("click", () => {
   drinkItems.classList.add("clipped");
   drinkItems.classList.remove("not-clipped");
   foodItems.classList.remove("clipped");
   foodItems.classList.add("not-clipped");
+  menuItems.style.height = "540px";
+  itemsSection.style.height = "540px";
 });
 
 drinksButtom.addEventListener("click", () => {
@@ -16,4 +21,6 @@ drinksButtom.addEventListener("click", () => {
   foodItems.classList.remove("not-clipped");
   drinkItems.classList.remove("clipped");
   drinkItems.classList.add("not-clipped");
+  menuItems.style.height = "1000px";
+  itemsSection.style.height = "1000px";
 });
